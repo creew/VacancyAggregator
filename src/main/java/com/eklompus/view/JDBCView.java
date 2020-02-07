d2918e97ae4b288dff87028b4ed53729a3f00263
creew <eklompus@student.21-school.ru>
2020-02-07 18:38:53 +0300

    added jdbc

---
+++ src/main/java/com/eklompus/view/JDBCView.java
@@ -0,0 +1,19 @@
+package com.eklompus.view;
+
+import com.eklompus.Controller;
+import com.eklompus.vo.Vacancy;
+
+import java.util.List;
+
+public class JDBCView implements View {
+
+    @Override
+    public void update(List<Vacancy> vacancies) {
+
+    }
+
+    @Override
+    public void setController(Controller controller) {
+
+    }
+}
